27bb25b9baf23bfff969cc18961139c775d2d344
Hey <brn8@njit.edu>
2024-12-02 12:46:15 -0500

    implemented Puppy Pals

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./App.css";
+import { puppyList } from "./data";
+
+function App() {
+  const [puppyInformation, setPuppyInformation] = useState(null);
+  return (
+    <>
+      <div className="card">
+        {puppyInformation && (
+          <div className="activeCard">
+            <h2 style={{ textAlign: "center" }}>{puppyInformation.name}</h2>
+            <p>
+              <strong>Email:</strong> {puppyInformation.email}
+            </p>
+            <p>
+              {" "}
+              <strong>Age:</strong> {puppyInformation.age}
+            </p>
+            {puppyInformation.tricks.length > 0 && (
+              <>
+                <p>
+                  <strong>Tricks: </strong>
+                </p>
+                <ul>
+                  {puppyInformation.tricks.map((trick) => (
+                    <li key={trick.id}>{trick.title}</li>
+                  ))}
+                </ul>
+              </>
+            )}
+          </div>
+        )}
+        <h2 style={{ textAlign: "center" }}>List of Puppies</h2>
+
+        {puppyList.map((puppy) => {
+          return (
+            <p
+              style={{ textAlign: "center" }}
+              key={puppy.id}
+              onClick={() => setPuppyInformation(puppy)}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
